95c7827b5d1d80fedb5335fa6c66cf13ff3d57bc
Seetharam1999 <srseetharam1999@gmail.com>
2020-09-06 13:12:57 +0530

    Fiter

--- Deals/Deals.js
+++ Deals/Deals.js
@@ -19,54 +19,60 @@ document.getElementById('footer').style.bottom="-50px"
 prevScrollpos = currentScrollPos;
 
     }   }
-    function toggle(data){
-        if(data==='S'){
-            window.location="../search.html";
-        }
-    }
+    
 function myFunction() {
 var x = document.getElementById("header");
 var y = document.getElementById("footer");
-var card=document.getElementById('main');
 if (x.style.display === "none") {
 x.style.display = "block";
-card.style.top="30px";
 x.style.top="0px"
 
 } else {
 x.style.display = "none";
-card.style.top="0px";
 }
 if (y.style.display === "none") {
 y.style.display = "block";
 y.style.bottom="0px"
-
 } else {
 y.style.display = "none";
 }
 }
 
-$(document).ready(()=>{
-    fetch('/js/header.html')
-    .then(data=>{
-        return data.text();
-    })
-    .then(dataJson=>{
-        document.getElementById('headerID').innerHTML=dataJson;
-    })
-    fetch('/js/footers.html')
-    .then(data=>{
-        return data.text()
-    })
-    .then(dataJSon=>{
-        document.getElementById('footer').innerHTML=dataJSon;
-    })
-    // fetch('/filter.html')
-    // .then(data=>{
-    //     return data.text()})
-    //     .then(dataJs=>{
-    //         document.getElementById('filter').innerHTML=dataJs
-    //     })
+// $(document).ready(()=>{
+//     fetch('/js/header.html')
+//     .then(data=>{
+//         return data.text();
+//     })
+//     .then(dataJson=>{
+//         document.getElementById('headerID').innerHTML=dataJson;
+//     })
+//     fetch('/js/footers.html')
+//     .then(data=>{
+//         return data.text()
+//     })
+//     .then(dataJSon=>{
+//         document.getElementById('footer').innerHTML=dataJSon;
+//     })
+//     // fetch('/filter.html')
+//     // .then(data=>{
+//     //     return data.text()})
+//     //     .then(dataJs=>{
+//     //         document.getElementById('filter').innerHTML=dataJs
+//     //     })
 
+// })
+
+//// Filter Section
+let checkbox = document.getElementById('cb');
+var svg = document.getElementById('checkbox');
+
+svg.addEventListener('click', () => {
+    if(checkbox.checked)
+        svg.classList.add('reverse')
+    else 
+        svg.classList.remove('reverse')
 })
 
+rest=()=>{
+    $('input[type=checkbox]').prop('checked', false);
+  };
