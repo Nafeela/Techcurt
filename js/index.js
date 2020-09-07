2a6332c348335eac09dd63b0d4873e9612f4e0ab
Seetharam1999 <srseetharam1999@gmail.com>
2020-09-07 15:59:39 +0530

    pwa

---
+++ js/index.js
@@ -0,0 +1,11 @@
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("/sw.js").then(registration=>{
+        console.log("SW registered");
+        console.log(registration)
+    })
+    .catch(error=>{
+        console.log("SW registration Failed");
+        console.log(error.message)
+    })
+
+}
---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install",e=>{
+    e.waitUntil(
+        caches.open('static').then(cache=>{
+            return cache.addAll(["./","./images/avatar3.png","./newspage/images/","./dealspage/images/"])
+        })
+    );
+    console.log(e)
+
+});
+self.addEventListener('fetch',e=>{
+   e.respondWith(
+       caches.match(e.request).then(response=>{
+    
+        return response|| fetch(e.request)
+    })
+   )
+})
