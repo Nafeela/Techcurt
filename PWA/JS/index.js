aeef9da4820833f7868a557ad4ddd7ef574eb1c1
Seetharam1999 <srseetharam1999@gmail.com>
2020-09-10 15:02:23 +0530

    PWA/NEWS

---
+++ PWA/JS/index.js
@@ -0,0 +1,12 @@
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("../sw.js").then(registration=>{
+        
+        console.log("SW registered PWA");
+        console.log(registration)
+    })
+    .catch(error=>{
+        console.log("SW registration Failed");
+        console.log(error.message)
+    })
+
+}
---
+++ PWA/sw.js
@@ -0,0 +1,54 @@
+const staticAccess=[
+    './',
+'./NEWS/index.html',
+'./NEWS/search.html',
+'./NEWS/filter1.html',
+'./images/copyright.png',
+'./images/home.png',
+'./images/all.png',
+'./images/news.png',
+'./images/product.png',
+'./images/review.png',
+'./images/best.png',
+'./images/root.png',
+'./images/deals.png',
+'./images/password.png',
+'./images/share.png',
+'./images/refresh.png',
+'./images/search1.png'];
+
+
+self.addEventListener("install",async e=>{
+    const cache=await caches.open('static');
+    cache.addAll(staticAccess)
+   
+});
+self.addEventListener('fetch',e=>{
+    const req=e.request;
+    const url=new URL(req.url);
+    if(url.origin===location.origin){
+        e.respondWith(cacheFirst(req));
+    }  
+    else{
+        e.respondWith(networkFirst(req))
+    }
+    
+
+});
+async function cacheFirst(req){
+    const cachedResponse=await caches.match(req);
+    return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req){
+    const cache=await caches.open('static');
+    try{
+        const req=await fetch(req);
+        cache.pull(req,res.clone());
+        return res;
+    }
+
+    catch(err){
+        return await cache.match(req);
+    }
+}
